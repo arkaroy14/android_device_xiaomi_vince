1eb8affbb0e2f3b41dde8c80c19961cd60a35c59
Arka Roy Chowdhury <arkaroyc21@gmail.com>
2020-02-06 18:22:43 +0530

    Some Changes

--- device.mk
+++ device.mk
@@ -56,3 +56,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
+# Wallpapers
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaperPrebuilt
--- havoc_vince.mk
+++ havoc_vince.mk
@@ -14,13 +14,6 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, vendor/havoc/config/phone-xxhdpi-4096-dalvik-heap.mk)
-$(call inherit-product, vendor/havoc/config/phone-xxhdpi-2048-hwui-memory.mk)
-
-
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
